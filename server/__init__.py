581a6cb8a3640d9a15db7b4c0a8ddcd512894966
eugene233 <agboreugene@gmail.com>
2023-04-26 15:02:34 +0100

    remove pool recycle

--- server/__init__.py
+++ server/__init__.py
@@ -14,7 +14,6 @@ class SQLAlchemy(_BaseSQLAlchemy):
     def apply_pool_defaults(self, app, options):
         super(SQLAlchemy, self).apply_pool_defaults(self, app, options)
         options["pool_pre_ping"] = True
-        options["pool_recycle"] = 300
 
 
 logging.basicConfig(
