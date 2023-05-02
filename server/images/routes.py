e6487ce2cd53d21d033e9bcdfceafe6222fe0fda
eugene233 <agboreugene@gmail.com>
2023-05-02 11:39:13 +0100

    Fetch image description

--- server/images/routes.py
+++ server/images/routes.py
@@ -1,5 +1,5 @@
-
-from flask import Blueprint, request, Response
+import requests
+from flask import Blueprint, request, Response 
 import json
 from flask_cors import cross_origin
 
@@ -8,15 +8,15 @@ from server.main.utils import get_serialized_data
 from server.models import Image, Category
 
 
-images = Blueprint('images', __name__)
+images = Blueprint("images", __name__)
 
 
-@images.route('/images')
+@images.route("/images")
 @cross_origin()
 def getImages():
-    category_name = request.args.get('category')
+    category_name = request.args.get("category")
     if not category_name:
-        return Response(status=404, response=json.dumps({'message': 'Category May be Invalid'}))
+        return Response(status=404, response=json.dumps({"message": "Category May be Invalid"}))
     category = Category.query.filter_by(name=category_name).first()
     all_images = Image.query.all()
     filtered_images = []
@@ -26,3 +26,27 @@ def getImages():
             filtered_images.append(image)
     return get_serialized_data(filtered_images)
 
+
+@images.route("/images/describe")
+@cross_origin()
+def getImageDescription():
+    print('were hit')
+    filename = request.args.get("filename")
+    if not filename:
+        return Response(status=404, response=json.dumps({"message": "Please provide a file name"}))
+    commons_url = "https://commons.wikimedia.org/w/api.php"
+    params = {
+        "action": "query",
+        "prop": "imageinfo",
+        "iiprop": "extmetadata",
+        "titles": filename,
+        "format": "json"
+    }
+    session = requests.Session()
+    resp = session.get(commons_url, params=params).json()
+    if not resp:
+        return Response(status=404, response=json.dumps({"message": "Could not get file descrription"}))
+    if resp['query']['pages'].keys():
+        page_id = list(resp['query']['pages'].keys())[0]
+        return resp['query']['pages'][page_id]['imageinfo'][0]['extmetadata']['ImageDescription']['value']
+    return ''
