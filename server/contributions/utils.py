b36ae16150152e81a79d7203938f32af1a81277c
eugene233 <agboreugene@gmail.com>
2023-06-29 11:06:21 +0100

    Modify contribution object

--- server/contributions/utils.py
+++ server/contributions/utils.py
@@ -6,23 +6,19 @@ def create_contribution(data):
     if not data['response']:
         return None
     identity_type = response.get('identity_type')
-    clarity=response.get('clarity')
     depict_accuracy=response.get('depict_accuracy')
     subject_relevance=response.get('subject_relevance')
-    accuracy=response.get('accuracy')
     region_alt=response.get('region_alt')
     region=response.get('region')
     representation=response.get('representation')
     username = data.get('username', 'Anonymous')
-    
+
     contribution = Contribution(
         filename=data['filename'],
         type=data['track'],
         identity_type=identity_type,
-        clarity=clarity,
         depict_accuracy=depict_accuracy,
         subject_relevance=subject_relevance,
-        accuracy=accuracy,
         region_alt=region_alt,
         region=region,
         representation=representation,
